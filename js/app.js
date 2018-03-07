d40afd6b94a8b437e2b28cbc1976ffb3bd867c7c
jmelnich <julyettka@gmail.com>
2018-03-07 13:26:48 +0200

    feat: implement draggable feature; fix: add id for each li and remove it from the array by its id

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 $(function() {
 	//check if I have smth in localStorage.
-	let storedData = localStorage.getItem("list_data_key");
+	let storedData = localStorage.getItem("list_tasks");
 	let data = {
-		tasks: []
+		tasks: [],
+		id: 0
 	};
 	if (storedData) {
+	console.log(storedData);
 	    data.tasks = JSON.parse(storedData);
+	    data.id = data.tasks.length;
 	}
 
 	let octopus = {
@@ -13,25 +16,28 @@ $(function() {
 		addTask: function(text) {
 			let utc = (new Date()).toString().split(' ').splice(1,4).join(' ');
 			data.tasks.push({
+				uid: data.id++,
 				time: utc,
 				text
-			})
+			});
 			view.render();
-			localStorage.setItem("list_data_key",  JSON.stringify(data.tasks));
-			console.log(localStorage.list_data_key);
+			localStorage.setItem("list_tasks",  JSON.stringify(data.tasks));
+			//console.log(localStorage.list_data_key);
 		},
 		getActiveTask: function() {
 			let activeTasks = data.tasks;
 			return activeTasks;
 		},
-		removeTask: function(node) {
-			data.tasks.splice(node, 1);
+		removeTask: function(id) {
+			let elem = data.tasks.map(obj => obj.uid == id);
+			let posInArr = elem.indexOf(true);
+			data.tasks.splice(posInArr, 1);
+			localStorage.setItem("list_tasks",  JSON.stringify(data.tasks));
 			view.render();
 		}
 	};
 
 	let view = {
-	// init function that grab btn and call octopus.addTask()
 		init: function() {
 			const addTaskBtn = $('.add-task');
 			$("#add").keyup(function(event) {
@@ -39,6 +45,7 @@ $(function() {
     				addTaskBtn.click();
     			}
     		});
+			//add task event handle
 			addTaskBtn.click(function() {
 			let inputFiledVal = $('#task-input').val();
 				if (inputFiledVal == ""){
@@ -50,12 +57,13 @@ $(function() {
 				}
 				$('#task-input').val("");
 			});
+			//remove task event handle
 			this.taskList = $('.task-list');
             this.taskTemplate = $('script[data-template="tasks"]').html();
             $('.task-list').on('click', '.remove-task', function(e) {
-                //let taskId = $(this).parents('.task').attr('id');
-                let nodeNb = $(this).parents('.task').index();
-                octopus.removeTask(nodeNb);
+                let taskId = $(this).parents('.task').attr('id');
+                //let nodeNb = $(this).parents('.task').index();
+                octopus.removeTask(taskId);
                 return false;
             });
 
@@ -64,11 +72,14 @@ $(function() {
 			let taskList = this.taskList,
 			taskTemplate= this.taskTemplate;
 			taskList.html('');
-			octopus.getActiveTask().map(function(task){
-				let thisTemplate = taskTemplate.replace(/{{text}}/g, task.text)
-												.replace(/{{date}}/g, task.time);
-				taskList.append(thisTemplate);
-			});
+			if (data.tasks){
+				octopus.getActiveTask().map(function(task){
+					let thisTemplate = taskTemplate.replace(/{{text}}/g, task.text)
+													.replace(/{{date}}/g, task.time)
+													.replace(/{{id}}/g, task.uid);
+					taskList.append(thisTemplate);
+				});
+			}
 		}
 	};
 	view.init();
